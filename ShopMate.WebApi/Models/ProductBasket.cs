9969a4fc198ed970e989f77d359bf27fa4ab6e8e
Victoria Fox <83537604+holyviktor@users.noreply.github.com>
2023-11-17 18:00:15 +0200

    adding and deleting product from basket

---
+++ ShopMate.WebApi/Models/ProductBasket.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopMate.WebApi.Models
+{
+    public class ProductBasket
+    {
+        public string ProductId { get; set; }
+        [Range(1, Int32.MaxValue)]
+        public int Count { get; set; }
+    }
+}
